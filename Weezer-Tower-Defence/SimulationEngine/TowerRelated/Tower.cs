6dc6cf67e5274d01e35c387b56a1afe6a879ad60
babadzakich <doctorare@mail.ru>
2025-11-19 02:17:50 +0700

    Add tower draw logic and template for update logic

--- Weezer-Tower-Defence/SimulationEngine/TowerRelated/Tower.cs
+++ Weezer-Tower-Defence/SimulationEngine/TowerRelated/Tower.cs
@@ -1,24 +1,44 @@
 using System.Collections.Generic;
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
 
 class Tower
 {
-    TowerType type;
+    public Vector2 Position { get; set; }
+    private TowerType _type;
+    // private List<Enemy> enemiesInRange;
+    private float _fireCooldown;
 
-    // List<Enemy> enemiesInRange;
-    float fireCooldown;
+    private Texture2D _sprite;
 
-    public Tower(TowerType type)
+    public Tower(TowerType type, Vector2 position)
     {
-        this.type = type;
+        _type = type;
+        Position = position;
         // enemiesInRange = new List<Enemy>();
-        fireCooldown = 0f;
+        _fireCooldown = 0f;
     }
 
-    public void Update(float deltaTime)
+    public void Update(GameTime gameTime)
     {
-        if (fireCooldown > 0)
+     _fireCooldown -= (float)gameTime.ElapsedGameTime.TotalSeconds;
+     if (_fireCooldown <= 0f)
         {
-            fireCooldown -= deltaTime;
+            
+        }   
+    }
+    public void Draw(SpriteBatch sb)
+    {
+        if (_sprite != null)
+            sb.Draw(_sprite, Position - new Vector2(_sprite.Width/2, _sprite.Height/2), Color.White);
+        else 
+        {
+            // Draw a placeholder rectangle if the sprite is not loaded
+            Texture2D placeholder = new Texture2D(sb.GraphicsDevice, 40, 40);
+            Color[] data = new Color[40 * 40];
+            for (int i = 0; i < data.Length; ++i) data[i] = Color.Gray;
+            placeholder.SetData(data);
+            sb.Draw(placeholder, Position - new Vector2(20, 20), Color.White);
         }
     }
 }
